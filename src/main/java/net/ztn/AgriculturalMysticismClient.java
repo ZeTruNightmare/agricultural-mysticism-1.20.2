ec3997854579c5f708e68b1786d3a68542c4ee2e
ZeTruNightmare <ZomgMinecraftable2@outlook.com>
2024-08-18 20:48:10 +0100

    Initial Push

---
+++ src/main/java/net/ztn/AgriculturalMysticismClient.java
@@ -0,0 +1,10 @@
+package net.ztn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AgriculturalMysticismClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
